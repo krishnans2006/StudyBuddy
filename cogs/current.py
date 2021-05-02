748c3a8d1e6bc80c5a607705ec14ac453cb0f921
Krishnan Shankar <>
2021-05-02 21:19:29 +0000

    Heroku Deployment

--- cogs/current.py
+++ cogs/current.py
@@ -19,16 +19,16 @@ class Current(commands.Cog, name="current", description="View your current tasks
     @commands.command(name="dashboard", aliases=["db"], brief="View your dashboard", description="View your upcoming classes, tasks, and exams!")
     async def dashboard(self, context):
         next10 = [
-            Class("Biology Honors", "Bio Honors Period 4! Room 68, Second Floor. Have fun learning Biology!", [ "7-8", "8-9","9-10","10-11", "11-12",None, None], "Mr. Jeff"),
-            Task("Homework Three", "Biology Honors", "Read Pages 23-31 in your textbook and take notes", datetime.now()),
+            Class("Biology Honors", "Bio Honors Period 4! Room 68, Second Floor. Have fun learning Biology!", [ None, "8-9","9-10","10-11", "11-12",None, None], "Mr. Jeff"),
+            Task("Homework Three", "Biology Honors", "Read Pages 23-31 in your textbook and take notes", datetime(2021, 5, 5)),
             Class("Pre-Calculus", "Pre-Calc Period 1! Room 45a, First Floor. Math is cool!", ["9-10","10-11","11-12",None,"8-9",None,None], "Ms. Russo"),
-            Task("Assignment Three", "Pre-Calculus", "Finish the problems on quadratic equations.", datetime.now()),
+            Task("Assignment Three", "Pre-Calculus", "Finish the problems on quadratic equations.", datetime(2021, 5, 6)),
             Class("American History", "History Period 2, Room 21, First Floor.", ["10-11","11-12",None,"8-9","9-10",None,None], "Mr. Singer"),
-            Task("Essay One", "American History","Finish your essays on the importance of the Madison v. Marbury case", datetime.now()),
-            Exam("History Quiz One", "American History", "Short quiz on the importance of Washington's presidential precident", datetime.now()),
+            Task("Essay One", "American History","Finish your essays on the importance of the Madison v. Marbury case", datetime(2021, 5, 7)),
+            Exam("History Quiz One", "American History", "Short quiz on the importance of Washington's presidential precident", datetime(2021, 5, 6, 8, 00)),
             Class("English Honors", "Honors English Period 3, at Room 33, First Floor. Please come prepared with notebooks, pencils, and your books.", ["11-12", None, "8-9","9-10","10-11",None,None], "Mrs. Berg"),
-            Task("Essay One","English Honors", "Please submit your rough drafts of your essay on the importance of Beowulf in Old English literature", datetime.now()),
-            Class("Biology Honors", "Bio Honors Period 4! Room 68, Second Floor. Have fun learning Biology!",[ "7-8", "8-9","9-10","10-11", "11-12",None, None], "Mr. Jeff")
+            Task("Essay One","English Honors", "Please submit your rough drafts of your essay on the importance of Beowulf in Old English literature", datetime(2021, 5, 10)),
+            Class("Biology Honors", "Bio Honors Period 4! Room 68, Second Floor. Have fun learning Biology!",[ None, "8-9","9-10","10-11", "11-12",None, None], "Mr. Jeff")
         ]  
         current_day = int(datetime.now().strftime("%w"))
         embed = create_embed(
@@ -43,24 +43,24 @@ class Current(commands.Cog, name="current", description="View your current tasks
                 ],
                 ["Date/Time", "\n\n".join(n.at[current_day] if isinstance(n.at, list) else str(n.at) for n in next10), True],
                 ["Teacher/Class", "\n\n".join(n.subject for n in next10), True],
-            ],
-            footer="Key: 🏫 Class, 📚 Task, 📝 Exam"
+                ["Key", "🏫 Class, 📚 Task, 📝 Exam"]
+            ]
         )
         await context.send(embed=embed)
     @commands.command(name = "tasks", aliases = ['view tasks','View Tasks'], 
     brief = "View your upcoming tasks", description = "View your next ten tasks!")
     async def viewTasks(self, context):
         next10 = [
-            Task("Homework Three", "Biology Honors", "Read Pages 23-31 in your textbook and take notes", datetime.now()),
-            Task("Assignment Three", "Pre-Calculus", "Finish the problems on quadratic equations.", datetime.now()),
-            Task("Essay One", "American History","Finish your essays on the importance of the Madison v. Marbury case", datetime.now()),
-            Task("Essay One","English Honors", "Please submit your rough drafts of your essay on the importance of Beowulf in Old English literature", datetime.now()),
-            Task("Homework Four", "Biology Honors", "Read Pages 34-45 in your textbook and take notes", datetime.now()),
-            Task("Assignment Four", "Pre-Calculus", "Finish the problems on quadratic equations.", datetime.now()),
-            Task("Reading Five", "American History", "Read the fifth chapter in your textbooks", datetime.now()),
-            Task("Reading Four","English Honors","Read the first story of The Canterbury Tales",datetime.now()),
-            Task("Homework Five", "Biology Honors", "Read Pages 48-60 in your textbook and take notes", datetime.now()),
-            Task("Assignment Five", "Pre-Calculus", "Finish the problems on logarithmic equations.", datetime.now()),
+            Task("Homework Three", "Biology Honors", "Read Pages 23-31 in your textbook and take notes", datetime(2021, 5, 5)),
+            Task("Assignment Three", "Pre-Calculus", "Finish the problems on quadratic equations.", datetime(2021, 5, 6)),
+            Task("Essay One", "American History","Finish your essays on the importance of the Madison v. Marbury case", datetime(2021, 5, 7)),
+            Task("Essay One","English Honors", "Please submit your rough drafts of your essay on the importance of Beowulf in Old English literature", datetime(2021, 5, 10)),
+            Task("Homework Four", "Biology Honors", "Read Pages 34-45 in your textbook and take notes", datetime(2021, 5, 11)),
+            Task("Assignment Four", "Pre-Calculus", "Finish the problems on quadratic equations.", datetime(2021, 5, 12)),
+            Task("Reading Five", "American History", "Read the fifth chapter in your textbooks", datetime(2021, 5, 13)),
+            Task("Reading Four","English Honors","Read the first story of The Canterbury Tales",datetime(2021, 5, 14)),
+            Task("Homework Five", "Biology Honors", "Read Pages 48-60 in your textbook and take notes", datetime(2021, 5, 17)),
+            Task("Assignment Five", "Pre-Calculus", "Finish the problems on logarithmic equations.", datetime(2021, 5, 18)),
         ]
         current_day = int(datetime.now().strftime("%w"))
         embed = create_embed(
@@ -75,24 +75,24 @@ class Current(commands.Cog, name="current", description="View your current tasks
                 ],
                 ["Date/Time", "\n\n".join(n.at[current_day] if isinstance(n.at, list) else str(n.at) for n in next10), True],
                 ["Teacher/Class", "\n\n".join(n.subject for n in next10), True],
-            ],
-            footer="Key: 🏫 Class, 📚 Task, 📝 Exam"
+                ["Key", "🏫 Class, 📚 Task, 📝 Exam"]
+            ]
             )
         await context.send(embed=embed)
     @commands.command(name = "exams", aliases = ['View Exams'], 
     brief = "View your upcoming tasks", description = "View your next ten exams!")
     async def viewExams(self, context):
         next10 = [
-            Exam("History Quiz One", "American History", "Short               quiz on the importance of Washington's presidential               precident", datetime.now()),
-            Exam("Calc Quiz One", "Pre-Calculus", "Quiz on      quadratic equations, bring your calculator!", datetime.now()),
-            Exam("English Quiz One", "English Honors", "Quiz on Beowulf.", datetime.now()),
-            Exam("Bio Quiz One", "Biology Honors", "Will cover the parts of the cell, open notes", datetime.now()),
-            Exam("Calc Quiz Two", "Pre-Calculus", "Quiz on logarithms and logarithmic equations.", datetime.now()),
-            Exam("English Essay One", "English Honors", "In class essay on the development of Middle English", datetime.now()),
-            Exam("History Quiz Two", "American History", "Short assessment of your understanding of the early presidents", datetime.now()),
-            Exam("Bio Quiz Two", "Biology Honors", "Short quiz on recent reading assignment", datetime.now()),
-            Exam("History Test One", "American History", "Covers the material thus far assigned", datetime.now()),
-            Exam("Calc Test One", "Pre-Calculus", "Covers the material learned this semester", datetime.now())
+            Exam("History Quiz One", "American History", "Short               quiz on the importance of Washington's presidential precident", datetime(2021, 5, 6, 8, 00)),
+            Exam("Calc Quiz One", "Pre-Calculus", "Quiz on      quadratic equations, bring your calculator!", datetime(2021, 5, 12, 11, 00)),
+            Exam("English Quiz One", "English Honors", "Quiz on Beowulf.", datetime(2021, 5, 7, 10, 00)),
+            Exam("Bio Quiz One", "Biology Honors", "Will cover the parts of the cell, open notes", datetime(2021, 5, 10, 9, 00)),
+            Exam("Calc Quiz Two", "Pre-Calculus", "Quiz on logarithms and logarithmic equations.", datetime(2021, 5, 19, 11, 00)),
+            Exam("English Essay One", "English Honors", "In class essay on the development of Middle English", datetime(2021, 5, 21, 10, 00)),
+            Exam("History Quiz Two", "American History", "Short assessment of your understanding of the early presidents", datetime.(2021, 5, 20, 8, 00)),
+            Exam("Bio Quiz Two", "Biology Honors", "Short quiz on recent reading assignment", datetime(2021, 5, 17, 9, 00)),
+            Exam("History Test One", "American History", "Covers the material thus far assigned", datetime(2021, 6, 3, 8, 00)),
+            Exam("Calc Test One", "Pre-Calculus", "Covers the material learned this semester", datetime(2021, 6, 2, 11,00))
         ]
         current_day = int(datetime.now().strftime("%w"))
         embed = create_embed(
@@ -106,8 +106,8 @@ class Current(commands.Cog, name="current", description="View your current tasks
                     ],
                 ["Date/Time", "\n\n".join(n.at[current_day] if isinstance(n.at, list) else str(n.at) for n in next10), True],
                 ["Teacher/Class", "\n\n".join(n.subject for n in next10), True],
-            ],
-            footer="Key: 🏫 Class, 📚 Task, 📝 Exam"
+                ["Key", "🏫 Class, 📚 Task, 📝 Exam"]
+            ]
         )
         await context.send(embed=embed)
     
--- helpers/data.py
+++ helpers/data.py
@@ -34,3 +34,40 @@ def execute_query(query, params, fetchall=True):
         return results
     else:
         return False
+
+
+def insert_course(name ,description ,days  ,teacher):
+        
+      conn = mysql.connector.connect(host="35.188.102.27",
+                                          db="community",
+                                          user="myinstance",
+                                          password="123456")
+            
+      if conn:
+        cursor = conn.cursor()
+        cursor.execute("insert into Courses (name, description, days, teacher_name) VALUES(%s,%s,%s,%s)",name ,description ,days,teacher)
+        conn.commit();
+        return True
+        
+      else:
+        print("Error while connecting to MySQL")
+        return False
+       
+
+def insert_tasks(name,subject,description,dueDate):
+        
+      conn = mysql.connector.connect(host="35.188.102.27",
+                                          db="community",
+                                          user="myinstance",
+                                          password="123456")
+            
+      if conn:
+        cursor = conn.cursor()
+        cursor.execute("insert into Tasks (task_name, subject, task_description, dueDate) VALUES(%s,%s,%s,%s)",name, subject, description, dueDate)
+        conn.commit();
+        return True
+        
+      else:
+        print("Error while connecting to MySQL")
+        return False
+       
